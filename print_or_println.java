bea67b52ca63bd86dd7eb339d12a2ab429a454a9
Muhammad Aqilul Muttaqin <aqilulmuttaqin16@gmail.com>
2023-11-07 19:09:32 +0700

    print and println

---
+++ print_or_println.java
@@ -0,0 +1,15 @@
+public class print_or_println {
+    public static void main(String[] args) {
+        // Mencetak teks "Disini hujan," diikuti dengan baris baru
+        System.out.println("Disini hujan, ");
+        // Mencetak teks "Mari pakai jas hujan." diikuti dengan baris baru
+        System.out.println("Mari pakai jas hujan.");
+        
+        System.out.println();
+        
+        // Mencetak teks "Disini hujan," tanpa baris baru
+        System.out.print("Disini hujan, ");
+        // Mencetak teks "Mari pakai jas hujan." tanpa baris baru
+        System.out.print("Mari pakai jas hujan.");
+    }
+}
